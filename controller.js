2529137b522369f3aeb22ef2af02d3fbb8181eca
Kaio <kaio.correia@alunoescolassociais.org.br>
2024-07-04 11:11:27 -0300

    função troca de pagina

--- controller.js
+++ controller.js
@@ -1,12 +1,11 @@
 // função para validação de acesso
-function acessar(){
-    let loginEmail = document.getElementById ('loginEmail').value;
-    let loginSenha = document.getElementById ('loginSenha').value;
-    if(! loginEmail || loginSenha){
-        alert('Favor preencher todos os campos');
-    }
-    else{
-        alert('Campos preenchidos com sucesso')
-    }
+function acessar() {
+  let loginEmail = document.getElementById("loginEmail").value;
+  let loginSenha = document.getElementById("loginSenha").value;
+  if (!loginEmail || !loginSenha) {
+    alert("Favor preencher todos os campos");
+  } else {
+    // alert("Campos preenchidos com sucesso");
+    window.location.href = "cadastro.html";
+  }
 }
-
