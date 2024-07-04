b8d273aeb4a9f8a38c7cb0c5e879b6712a677f4d
Kaio <kaio.correia@alunoescolassociais.org.br>
2024-07-04 10:59:08 -0300

    adição função pegar valor dos campos email e senha

--- controller.js
+++ controller.js
@@ -1,1 +1,11 @@
-            
+// função para validação de acesso
+function acessar(){
+    let loginEmail = document.getElementById ('loginEmail').value;
+    let loginSenha = document.getElementById ('loginSenha').value;
+    if(! loginEmail || loginSenha){
+        alert('Favor preencher todos os campos');
+    }
+    else{
+        alert('Campos preenchidos com sucesso')
+    }
+}
