5a5f1c815e04abe8536706b2be6b5bc655b9b1dc
Kaio <kaio.correia@alunoescolassociais.org.br>
2024-08-16 09:20:16 -0300

    adição campo email

--- controller.js
+++ controller.js
@@ -9,10 +9,11 @@ function acessar() {
     window.location.href = "cadastro.html";
   }
 }
-// função para armazenamento de nomes em array
 var dadosListas = [];
+var dadosListasEmail = [];
+// função para armazenamento de nomes em array
 function salvarUser() {
-//  pega o valor digitado no input
+  //  pega o valor digitado no input
   let nomeUser = document.getElementById("nomeUser").value;
   if (nomeUser) {
     dadosListas.push(nomeUser);
@@ -23,18 +24,26 @@ function salvarUser() {
   } else {
     alert("Favor informe um nome para cadastro");
   }
+  //  pega o valor digitado no input
+ let EmailUser = document.getElementById("EmailUser").value;
+ if (EmailUser) {
+   dadosListasEmail.push(EmailUser);
+   console.log(dadosListasEmail);
+   criaLista();
+   // dexa o campo vazio para ser digitado novamente
+   document.getElementById("EmailUser").value = "";
+ } else {
+   alert("Favor informe um email para cadastro");
+ }
 }
 //  função para criar listas de usuario
 function criaLista() {
   //  pega o id da tabela no html
   let tabela = (document.getElementById("tabela").innerHTML =
-    "<tr><th>Nome Usúario</th><th>Ações</th></tr>");
+    "<tr><th>Nome Usúario</th><th>Email</th><th>Ações</th></tr>");
   // pega o valor do id nomeUser e cria uma coluna na tabela
-  for (let i = 0; i <= dadosListas.length - 1; i++) {
-    tabela +=
-      "<tr><td>" +
-      dadosListas[i] +
-      "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)' class='btn btn-dark'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)' class='btn btn-dark'>excluir</button></td></tr>";
+  for (let i = 0; i <= dadosListas.length - 1; i++){
+    tabela += "<tr><td>" + dadosListas[i] + "</td>"+ "<td>" + dadosListasEmail[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)' class='btn btn-dark'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)' class='btn btn-dark'>excluir</button></td></tr>";
     document.getElementById("tabela").innerHTML = tabela;
   }
 }
