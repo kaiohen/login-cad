56170955a53f952527c77a365a2b045a0668e51d
kaiohen <160147910+kaiohen@users.noreply.github.com>
2024-08-25 13:18:32 -0300

    validação email usuário da tabela

--- controller.js
+++ controller.js
@@ -26,22 +26,35 @@ function salvarUser() {
   }
   //  pega o valor digitado no input
  let EmailUser = document.getElementById("EmailUser").value;
- if (EmailUser) {
+ if (EmailUser && checarEmail()) {
    dadosListasEmail.push(EmailUser);
    console.log(dadosListasEmail);
    criaLista();
    // dexa o campo vazio para ser digitado novamente
    document.getElementById("EmailUser").value = "";
  } else {
-   alert("Favor informe um email para cadastro");
+   // Se o email for inválido ou vazio, remove o email adicionado
+       dadosListasEmail.pop();
+    alert("Email inválido.");
  }
+}
+// validação do email no cadastro de usuario
+function checarEmail() {
+  let email = document.getElementById("EmailUser").value;
+  // verifica se oque foi digitado é um email
+  if (email == "" || email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+    return false;
+  } else {
+    // se for um email valido aparecera na tabela
+    return true;
+  }
 }
 //  função para criar listas de usuario
 function criaLista() {
   //  pega o id da tabela no html
   let tabela = (document.getElementById("tabela").innerHTML =
     "<tr><th>Nome Usúario</th><th>Email</th><th>Ações</th></tr>");
-  // pega o valor do id nomeUser e cria uma coluna na tabela
+  // pega o valor do id nomeUser e emailUser e cria uma coluna na tabela
   for (let i = 0; i <= dadosListas.length - 1; i++){
     tabela += "<tr><td>" + dadosListas[i] + "</td>"+ "<td>" + dadosListasEmail[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)' class='btn btn-dark'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)' class='btn btn-dark'>excluir</button></td></tr>";
     document.getElementById("tabela").innerHTML = tabela;
